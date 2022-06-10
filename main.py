1906354a3a85f98e7c85076eb264ac94ab775191
pvhawk87@gmail.com <pvhawk87@gmail.com>
2022-06-10 17:41:04 +0600

    hw3 scrapping hard

--- main.py
+++ main.py
@@ -14,17 +14,20 @@ import re
 
 
 if __name__ == "__main__":
-
     res = requests.get(URL_BASE + URL, headers=HEADERS)
     soup = BeautifulSoup(res.text, 'html.parser')
     posts = soup.find_all('article', class_='tm-articles-list__item')
     pattern = '[\w]+'
     for post in posts:
-        reviews = post.find('div', class_='article-formatted-body').text
-        result = re.findall(pattern, reviews, re.U)
+        href = post.find('a', class_='tm-article-snippet__title-link').attrs["href"]
+        res2 = requests.get(URL_BASE + href, headers=HEADERS)
+        soup2 = BeautifulSoup(res2.text, 'html.parser')
+        texts = soup2.find('div', class_='article-formatted-body').text
+        result = re.findall(pattern, texts, re.U)
         set_KEY = set(KEYWORDS)
-        set_RES = set(result)
-        if not set_RES.isdisjoint(set_KEY):
+        set_TEXT = set(result)
+
+        if not set_TEXT.isdisjoint(set_KEY):
             date = post.find('span', class_='tm-article-snippet__datetime-published').find('time').attrs["datetime"]
             title = post.find('h2', class_='tm-article-snippet__title').text
             href = post.find('a', class_='tm-article-snippet__title-link').attrs["href"]
